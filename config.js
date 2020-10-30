8b19480cc4b4ea4741302910c00ff4fe7cb2f7b3
Dalong <dalonghu777@gmail.com>
2020-10-30 15:25:07 -0400

    upload files from my computer

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         '5O6IPxfQAeqFRT8amMwR9fgeW',
+  consumer_secret:      '0pzZQpEaJgwa5cv4Z2YpcPtrLBncDJfOFC0IM3cqIBZDcvbEEU',
+  access_token:         '1321587704866443264-VCHc9MlEeOZvjdJLMB994dhpzA6W9f',
+  access_token_secret:  'EC6e2SFOYi22v1urmSKISimJ91D7zx61yxF917vnUPg05'
+}
